a16fc36df6941449a140656fdd0b8c578b69808e
Void-ux <daniel.grant2017@gmail.com>
2022-12-01 23:22:31 +0800

    first commit

---
+++ 2022/day_1.py
@@ -0,0 +1,10 @@
+with open('day_1.txt', 'r') as file:
+    content = file.read()
+
+elves = content.split('\n\n')
+print(max(sum(int(cals) for cals in elf.split('\n')) for elf in elves))
+
+# Part 2
+calories = [sum(int(cals) for cals in elf.split('\n')) for elf in elves]
+calories.sort(reverse=True)
+print(sum(calories[:3]))
